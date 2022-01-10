102b69d3e3357a058b6682a88fae34e8c00a5c18
unknown <erfan8202@gmail.com>
2022-01-10 20:13:43 +0330

    toast bug in mobile(rtl) fixed

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ const App = () => {
                             }/>
                         </Switch>
                         </BrowserRouter>
-                        <ToastContainer/>
+                        <ToastContainer rtl/>
                         </>
                         );
                     };
