07e154201de604391d85d59bc88ba398f94d807a
unknown <erfan8202@gmail.com>
2022-01-31 04:04:07 +0330

    icons style changed

--- src/pages/auth/styles.js
+++ src/pages/auth/styles.js
@@ -1,10 +1,12 @@
 import {makeStyles} from "@material-ui/styles";
 
 const useStyles = makeStyles(theme => ({
+
     container: {
         background: "white",
+        overflow: 'hidden',
         width: "30rem",
-        margin: "10rem auto",
+        margin: "4.5rem auto",
         display: "flex",
         flexDirection: "column",
         [theme.breakpoints.down("xs")]: {
@@ -13,9 +15,9 @@ const useStyles = makeStyles(theme => ({
         direction: 'rtl',
         boxShadow: '0 8px 32px 0 rgb(31 38 135 / 37%)'
     },
-    headerText: {
-        margin: '1rem',
+    logo: {
         alignSelf: 'center',
+        width: '80px'
     },
     tab: {
         flex: 1,
@@ -23,9 +25,33 @@ const useStyles = makeStyles(theme => ({
         fontFamily: 'shabnam !important'
     },
     containerInput: {
-        padding: '1rem 0.8rem',
+        padding: '1rem 0.8rem 0.1rem 0.8rem',
         display: 'flex',
         flexDirection: 'column'
+    },
+    markIcon: {
+        marginRight: '-11px'
+    },
+    socialMedia: {
+        marginTop: '20px',
+    },
+    divider: {
+        marginLeft: '10%',
+        marginRight: '10%'
+    },
+    dividerText: {
+        textAlign: 'center',
+        marginTop: '5px'
+    },
+    iconsParent: {
+        textAlign: "center",
+        margin: '10px',
+        padding: '10px'
+    },
+    icon: {
+        margin: '0 10px 0 10px',
+        width: '30px',
+        height: '30px'
     }
 }));
 
