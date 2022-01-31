81dc3f29f40081fa2d6fad8fe0c27e721f492132
unknown <erfan8202@gmail.com>
2022-01-31 04:02:56 +0330

    icons added

--- src/pages/auth/AuthPage.js
+++ src/pages/auth/AuthPage.js
@@ -10,143 +10,170 @@ import {toast} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import {loginApi, registerApi} from "../../api/api_auth";
 import {useTranslation} from "react-i18next";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import {Checkbox, FormControlLabel} from "@material-ui/core";
+import {Favorite, FavoriteBorder} from "@material-ui/icons";
 
 const LOGIN_TAB_VALUE = 1;
 const REG_TAB_VALUE = 2;
 
 const AuthPage = () => {
-  const classes = useStyles();
-  const {t} = useTranslation();
+    const classes = useStyles();
+    const {t} = useTranslation();
 
-  const [tab, setTab] = useState(LOGIN_TAB_VALUE);
+    const [tab, setTab] = useState(LOGIN_TAB_VALUE);
 
-  //login state
-  const [usernameLogin, setUsernameLogin] = useState();
-  const [passwordLogin, setPasswordLogin] = useState();
+    //login state
+    const [usernameLogin, setUsernameLogin] = useState();
+    const [passwordLogin, setPasswordLogin] = useState();
 
-  //register state
-  const [fullNameRegister, setFullNameRegister] = useState();
-  const [usernameRegister, setUsernameRegister] = useState();
-  const [passwordRegister, setPasswordRegister] = useState();
-  const [confPasswordRegister, setConfPasswordRegister] = useState();
+    //register state
+    const [fullNameRegister, setFullNameRegister] = useState();
+    const [usernameRegister, setUsernameRegister] = useState();
+    const [passwordRegister, setPasswordRegister] = useState();
+    const [confPasswordRegister, setConfPasswordRegister] = useState();
 
 
-  const handleChangeTab = (e, newValue) => {
-    setTab(newValue);
-  };
+    const handleChangeTab = (e, newValue) => {
+        setTab(newValue);
+    };
 // validate login
-  const validateLogin = (user) => {
-    if (!user.username)
-      return t("validate.userName");
-    if (!user.password)
-      return t("validate.password")
-  };
-  // validate register
-  const validateRegister = (user) => {
-    if (!user.username)
-      return t("validate.userName");
-    if (!user.name)
-      return t("validate.name");
-    if (!user.password)
-      return t("validate.password");
-    if (user.password !== user.confPasswordRegister)
-      return t("validate.confPassword")
-  };
+    const validateLogin = (user) => {
+        if (!user.username)
+            return t("validate.userName");
+        if (!user.password)
+            return t("validate.password")
+    };
+    // validate register
+    const validateRegister = (user) => {
+        if (!user.username)
+            return t("validate.userName");
+        if (!user.name)
+            return t("validate.name");
+        if (!user.password)
+            return t("validate.password");
+        if (user.password !== user.confPasswordRegister)
+            return t("validate.confPassword")
+    };
 // handle Register
-  const handleRegister = () => {
-    const user = {
-      name: fullNameRegister,
-      username: usernameRegister,
-      password: passwordRegister,
-      confPasswordRegister: confPasswordRegister,
+    const handleRegister = () => {
+        const user = {
+            name: fullNameRegister,
+            username: usernameRegister,
+            password: passwordRegister,
+            confPasswordRegister: confPasswordRegister,
+        };
+        const error = validateRegister(user);
+        if (error)
+            return toast.warn(error);
+        user.confPasswordRegister = undefined;
+        //set local storage after Register
+        registerApi(user, (isOk, data) => {
+            if (!isOk)
+                return toast.error(data);
+            toast.success(t("success.register"));
+            localStorage.setItem("name", data.name);
+            localStorage.setItem("image", data.image);
+            localStorage.setItem("username", data.username);
+            localStorage.setItem("x-auth-token", data["x-auth-token"]);
+            window.location.reload();
+        })
     };
-    const error = validateRegister(user);
-    if (error)
-      return toast.warn(error);
-    user.confPasswordRegister = undefined;
-    //set local storage after Register
-    registerApi(user, (isOk, data) => {
-      if (!isOk)
-        return toast.error(data);
-      toast.success(t("success.register"));
-      localStorage.setItem("name", data.name);
-      localStorage.setItem("image", data.image);
-      localStorage.setItem("username", data.username);
-      localStorage.setItem("x-auth-token", data["x-auth-token"]);
-      window.location.reload();
-    })
-  };
-  // handle login
-  const handleLogin = () => {
-    const user = {
-      username: usernameLogin,
-      password: passwordLogin
+    // handle login
+    const handleLogin = () => {
+        const user = {
+            username: usernameLogin,
+            password: passwordLogin
+        };
+        const error = validateLogin(user);
+        if (error)
+            return toast.warn(error);
+        loginApi(user, (isOk, data) => {
+            if (!isOk)
+                return toast.error(data);
+            //set local storage after login
+            toast.success(t("success.login"));
+            localStorage.setItem("name", data.name);
+            localStorage.setItem("image", data.image);
+            localStorage.setItem("username", data.username);
+            localStorage.setItem("x-auth-token", data["x-auth-token"]);
+            window.location.reload();
+        })
     };
-    const error = validateLogin(user);
-    if (error)
-      return toast.warn(error);
-    loginApi(user, (isOk, data) => {
-      if (!isOk)
-        return toast.error(data);
-      //set local storage after login
-      toast.success(t("success.login"));
-      localStorage.setItem("name", data.name);
-      localStorage.setItem("image", data.image);
-      localStorage.setItem("username", data.username);
-      localStorage.setItem("x-auth-token", data["x-auth-token"]);
-      window.location.reload();
-    })
-  };
 
-  return (
-    <Paper className={classes.container}>
-      <Typography className={classes.headerText}>{t("welcome")}</Typography>
-      <Tabs
-        value={tab}
-        indicatorColor="primary"
-        textColor="primary"
-        onChange={handleChangeTab}
-      >
-        <Tab label={t("tab.login")} value={LOGIN_TAB_VALUE} className={classes.tab}/>
-        <Tab label={t("tab.register")} value={REG_TAB_VALUE} className={classes.tab}/>
-      </Tabs>
-      {tab === LOGIN_TAB_VALUE &&
-      <div className={classes.containerInput}>
-        <Typography>{t("label.username")}</Typography>
-        <Input className={"uni_m_b_small"}
-               value={usernameLogin} onChange={e => setUsernameLogin(e.target.value)}
-        >
-        </Input>
-        <Typography>{t("label.password")}</Typography>
-        <Input className={"uni_m_b_small"}
-               value={passwordLogin} onChange={e => setPasswordLogin(e.target.value)}
-        />
-        <Button variant={"contained"} color="primary" onClick={handleLogin}>{t("btn.login")}</Button>
-      </div>
-      }
-      {tab === REG_TAB_VALUE &&
-      <div className={classes.containerInput}>
-        <Typography>{t("label.fullName")}</Typography>
-        <Input className={"uni_m_b_small"}
-               value={fullNameRegister} onChange={e => setFullNameRegister(e.target.value)}
-        />
-        <Typography>{t("label.username")}</Typography>
-        <Input className={"uni_m_b_small"}
-               value={usernameRegister} onChange={e => setUsernameRegister(e.target.value)}
-        />
-        <Typography>{t("label.password")}</Typography>
-        <Input className={"uni_m_b_small"}
-               value={passwordRegister} onChange={e => setPasswordRegister(e.target.value)}
-        />
-        <Typography>{t("label.confPassword")}</Typography>
-        <Input className={"uni_m_b_small"}
-               value={confPasswordRegister} onChange={e => setConfPasswordRegister(e.target.value)}
-        />
-        <Button variant={"contained"} color="primary" onClick={handleRegister}>{t("btn.register")}</Button>
-      </div>
-      }
-    </Paper>
-  );
+    return (
+
+            <Paper className={classes.container}>
+                <img className={classes.logo} src={"/images/twitter.png"}/>
+                <Tabs
+                    value={tab}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    onChange={handleChangeTab}
+                >
+                    <Tab label={t("tab.login")} value={LOGIN_TAB_VALUE} className={classes.tab}/>
+                    <Tab label={t("tab.register")} value={REG_TAB_VALUE} className={classes.tab}/>
+                </Tabs>
+                {tab === LOGIN_TAB_VALUE &&
+                <div className={classes.containerInput}>
+                    <Typography>{t("label.username")}</Typography>
+                    <Input className={"uni_m_b_small"}
+                           value={usernameLogin} onChange={e => setUsernameLogin(e.target.value)}
+                    >
+                    </Input>
+                    <Typography>{t("label.password")}</Typography>
+                    <Input className={"uni_m_b_small"}
+                           value={passwordLogin} onChange={e => setPasswordLogin(e.target.value)}
+                    />
+                    <FormControlLabel className={classes.markIcon}
+                        control={<Checkbox color="primary"/>}
+                        label={t("label.reminder")}
+                    />
+                    <Button variant={"contained"} color="primary" onClick={handleLogin}>{t("btn.login")}</Button>
+                </div>
+                }
+                {tab === REG_TAB_VALUE &&
+                <div className={classes.containerInput}>
+                    <Typography>{t("label.fullName")}</Typography>
+                    <Input className={"uni_m_b_small"}
+                           value={fullNameRegister} onChange={e => setFullNameRegister(e.target.value)}
+                    />
+                    <Typography>{t("label.username")}</Typography>
+                    <Input className={"uni_m_b_small"}
+                           value={usernameRegister} onChange={e => setUsernameRegister(e.target.value)}
+                    />
+                    <Typography>{t("label.password")}</Typography>
+                    <Input className={"uni_m_b_small"}
+                           value={passwordRegister} onChange={e => setPasswordRegister(e.target.value)}
+                    />
+                    <Typography>{t("label.confPassword")}</Typography>
+                    <Input className={"uni_m_b_small"}
+                           value={confPasswordRegister} onChange={e => setConfPasswordRegister(e.target.value)}
+                    />
+                    <Button variant={"contained"} color="primary" onClick={handleRegister}>{t("btn.register")}</Button>
+                </div>
+                }
+                <div className={classes.socialMedia}>
+                    <Divider variant={"middle"} className={classes.divider}/>
+                    <Typography className={classes.dividerText}> {t("label.or")}</Typography>
+                    <div className={classes.iconsParent}>
+                        <a href={"https://www.instagram.com/erfan82.p/"}>
+                            <img className={classes.icon} src={"/images/instagram.png"}/>
+                        </a>
+                        <a href={"https://www.instagram.com/erfan82.p/"}>
+                            <img className={classes.icon} src={"/images/google.png"}/>
+                        </a>
+                        <a href={"https://www.instagram.com/erfan82.p/"}>
+                            <img className={classes.icon} src={"/images/linkedin.png"}/>
+                        </a>
+                        <a href={"https://www.instagram.com/erfan82.p/"}>
+                            <img className={classes.icon} src={"/images/facebook.png"}/>
+                        </a>
+                    </div>
+                </div>
+            </Paper>
+    );
 };
 
 export default AuthPage;
