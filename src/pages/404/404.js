581c5a5304ded2604e7b11e8e8180f00843618cd
unknown <erfan8202@gmail.com>
2022-06-15 18:11:02 +0430

    update

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {useTranslation} from "react-i18next";
+import useStyle from './styles'
+
+
 
 const P404= () => {
-
-  const {t}= useTranslation()
+    const {t}= useTranslation()
+    const classes = useStyle();
 
   return (
     <div>
-      <h1>404</h1>
-      <p>{t("404_desc")}</p>
+        <img className={classes.notFoundImg} src={"/images/404.png"}/>
+        <div className={classes.notFoundText}>
+        {t("404_desc")}
+        </div>
     </div>
-  );
-};
+  )};
 
 export default P404;
