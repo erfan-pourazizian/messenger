f07493356ae97358192c83d26a164e296844b0c2
unknown <erfan8202@gmail.com>
2022-06-15 18:04:36 +0430

    404 page rebuild

---
+++ src/pages/404/styles.js
@@ -0,0 +1,14 @@
+import {makeStyles} from "@material-ui/styles";
+
+const useStyles = makeStyles({
+    notFoundImg: {
+        width : '100%'
+    },
+    notFoundText: {
+        fontSize: '19px',
+        textAlign: 'center'
+    }
+});
+
+
+export default useStyles;
