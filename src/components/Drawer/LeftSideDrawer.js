40fc36562d21e81e66b5362c1f7e5cb37b2939e9
unknown <erfan8202@gmail.com>
2022-06-15 15:57:58 +0430

    db json removed

--- src/components/Drawer/LeftSideDrawer.js
+++ src/components/Drawer/LeftSideDrawer.js
@@ -1,16 +1,17 @@
-
 import React from 'react';
 import {Drawer} from "@material-ui/core";
 import LeftSidebar from "../leftSidebar/LeftSidebar";
 import {toggleLeftDrawer, useLayoutDispatch, useLayoutState} from "../../context/LayoutContext";
 
 
-const LeftSideDrawer = ()=>{
-const {leftDrawerOpen}= useLayoutState()
-const  Dispatch= useLayoutDispatch()
+const LeftSideDrawer = () => {
+    const {leftDrawerOpen} = useLayoutState()
+    const Dispatch = useLayoutDispatch()
 
-    return  <Drawer anchor={"left"} open={leftDrawerOpen} onClose={()=>{toggleLeftDrawer(Dispatch)}}>
-       <LeftSidebar/>
+    return <Drawer anchor={"left"} open={leftDrawerOpen} onClose={() => {
+        toggleLeftDrawer(Dispatch)
+    }}>
+        <LeftSidebar/>
     </Drawer>
 }
 
